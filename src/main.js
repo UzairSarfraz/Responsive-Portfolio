039681f8d50e96cfbaa6899a81f85824ac8445ca
Uzair Sarfraz <116909186+UzairSarfraz@users.noreply.github.com>
2023-06-25 08:00:52 +0500

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import '../styles/modern-normalize.css';
+import '../tyles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/components/about.css';
+import './styles/utils.css';
